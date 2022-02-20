2f48839d3d0c31b4352f54bccccaa8a9e9444b18
Kang Kujun <donato14@naver.com>
2022-02-20 16:02:48 +0900

    🎨 Organizing the cords

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,18 +3,6 @@ import TodoInsert from './components/TodoInsert';
 import TodoTemplate from './components/TodoTemplate';
 import TodoList from './components/TodoList';
 
-function createBulkTodos() {
-  const array = [];
-  for (let i = 1; i <= 2500; i++) {
-    array.push({
-      id: i,
-      text: `할 일 ${i}`,
-      checked: false,
-    });
-  }
-  return array;
-}
-
 const App = () => {
   const [todos, setTodos] = useState([
     {
